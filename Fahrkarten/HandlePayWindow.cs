c9f8f89c5ed7f3dd45044a81ebda4c20d6569a7d
Nakiface <BernhardSchmitt@googlemail.com>
2020-09-18 15:06:23 +0200

    button add

--- Fahrkarten/HandlePayWindow.cs
+++ Fahrkarten/HandlePayWindow.cs
@@ -24,6 +24,34 @@ namespace Fahrkarten
             pay.Button500.Click += Button500_Click;
             pay.Button200.Click += Button200_Click;
             pay.Button100.Click += Button100_Click;
+            pay.Button2000.Click += Button2000_Click;
+            pay.Button50.Click += Button50_Click;
+            pay.Button20.Click += Button20_Click;
+            pay.Button10.Click += Button10_Click;
+        }
+
+        private void Button10_Click(object sender, RoutedEventArgs e)
+        {
+            Credit += 0.10m;
+            RefreshCredit();
+        }
+
+        private void Button20_Click(object sender, RoutedEventArgs e)
+        {
+            Credit += 0.20m;
+            RefreshCredit();
+        }
+
+        private void Button50_Click(object sender, RoutedEventArgs e)
+        {
+            Credit += 0.50m;
+            RefreshCredit();
+        }
+
+        private void Button2000_Click(object sender, RoutedEventArgs e)
+        {
+            Credit += 20.00m;
+            RefreshCredit();
         }
 
         private void Button100_Click(object sender, RoutedEventArgs e)
